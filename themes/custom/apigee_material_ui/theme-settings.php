1bd0deb0ca0b6a673553087f3be82f68b74a6d70
Chris Novak <chrisnovak@google.com>
2019-02-13 09:50:41 -0800

    Code style fixes
    * Fixed code style to match Drupal coding conventions
    * Removed commented out code

--- themes/custom/apigee_material_ui/theme-settings.php
+++ themes/custom/apigee_material_ui/theme-settings.php
@@ -1,67 +1,67 @@
 <?php
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Theme\ThemeSettings;
-use Drupal\system\Form\ThemeSettingsForm;
-use Drupal\Core\Form;
 
-function apigee_material_ui_form_system_theme_settings_alter(&$form, Drupal\Core\Form\FormStateInterface $form_state) {
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function apigee_material_ui_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
 
-  // Vertical tabs
-  $form['material'] = array(
+  // Vertical tabs.
+  $form['material'] = [
     '#type' => 'vertical_tabs',
     '#prefix' => '<h2><small>' . t('Material UI Settings') . '</small></h2>',
     '#weight' => -10,
-  );
-    // Navbar
-    $form['navbar'] = array(
-      '#type' => 'details',
-      '#title' => t('Navbar'),
-      '#group' => 'material',
-    );
-    $form['navbar']['fixed'] = array(
-      '#type' => 'details',
-      '#title' => t('Fixed Navbar'),
-      '#collapsible' => TRUE,
-      '#collapsed' => TRUE,
-    );
-    $form['navbar']['fixed']['material_ui_navbar_fixed'] = array(
-      '#type' => 'checkbox',
-      '#title' => t('Use Fixed navbar'),
-      '#default_value' => theme_get_setting('material_ui_navbar_fixed'),
-      '#description' => t('Select this checkbox to make the navbar always show on top')
-    );
-    //Layout
-  $form['layout'] = array(
+  ];
+  // Navbar.
+  $form['navbar'] = [
     '#type' => 'details',
-    '#title' => t('Layout '),
+    '#title' => t('Navbar'),
     '#group' => 'material',
-  );
-  $form['layout']['container'] = array(
+  ];
+  $form['navbar']['fixed'] = [
+    '#type' => 'details',
+    '#title' => t('Fixed Navbar'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  ];
+  $form['navbar']['fixed']['material_ui_navbar_fixed'] = [
+    '#type' => 'checkbox',
+    '#title' => t('Use Fixed navbar'),
+    '#default_value' => theme_get_setting('material_ui_navbar_fixed'),
+    '#description' => t('Select this checkbox to make the navbar always show on top'),
+  ];
+  // Layout.
+  $form['layout'] = [
+    '#type' => 'details',
+    '#title' => t('Layout'),
+    '#group' => 'material',
+  ];
+  $form['layout']['container'] = [
     '#type' => 'details',
     '#title' => t('Container'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
-  );
-  $form['layout']['container']['material_ui_container'] = array(
+  ];
+  $form['layout']['container']['material_ui_container'] = [
     '#type' => 'checkbox',
     '#title' => t('Contain content instead of full width'),
     '#default_value' => theme_get_setting('material_ui_container'),
-    '#description' => t('Set the content area to be contained by checking this box')
-  );
+    '#description' => t('Set the content area to be contained by checking this box'),
+  ];
 
-  // Setting colors
-  $form['colors'] = array(
+  // Setting colors.
+  $form['colors'] = [
     '#type' => 'details',
-    '#title' => t('Colors '),
+    '#title' => t('Colors'),
     '#group' => 'material',
-  );
-  $form['colors']['color_options'] = array(
+  ];
+  $form['colors']['color_options'] = [
     '#type' => 'details',
     '#title' => t('Color Options'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
-  );
+  ];
   $form['colors']['color_options']['material_ui_colors'] = [
     '#type' => 'select',
     '#title' => t('Choose primary colors'),
@@ -74,12 +74,10 @@ function apigee_material_ui_form_system_theme_settings_alter(&$form, Drupal\Core
       'green' => t('Green'),
       'grey darken-4' => t('Dark'),
       'white' => t('White'),
-      'transparent' => t('Transparent')
-    ]
+      'transparent' => t('Transparent'),
+    ],
   ];
-
 
 
 }
 
-?>
