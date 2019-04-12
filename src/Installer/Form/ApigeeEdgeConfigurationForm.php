1830dfbc71b1d0d3a78614d069b36e60ab0c419f
Arshad Chummun <arshad@chummun.com>
2019-04-12 20:06:33 +0400

    [DRUP-529] add an install task to configure apigee edge

---
+++ src/Installer/Form/ApigeeEdgeConfigurationForm.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @file
+ * Copyright 2019 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_devportal_kickstart\Installer\Form;
+
+use Drupal\apigee_edge\Form\AuthenticationForm;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configuration form for Apigee Edge.
+ */
+class ApigeeEdgeConfigurationForm extends AuthenticationForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    // Add a form title for the installer.
+    $form['#title'] = $this->t('Configure Apigee Edge');
+
+    // Hide the test_connection fields.
+    $form['test_connection']['#access'] = FALSE;
+
+    // Add a skip this step button.
+    $form['actions']['skip'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Skip this step'),
+      '#submit' => [[$this, 'skipStepSubmit']],
+      '#validate' => [],
+      '#limit_validation_errors' => [],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Do validation if not skip button.
+    if ((string) $form_state->getValue('op') !== (string) $form['actions']['skip']['#value']) {
+      parent::validateForm($form, $form_state);
+    }
+  }
+
+  /**
+   * Provides a submit handler for the skip step button.
+   *
+   * @param array $form
+   *   The form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   */
+  public function skipStepSubmit(array $form, FormStateInterface $form_state) {
+    global $install_state;
+    $install_state['completed_task'] = install_verify_completed_task();
+  }
+
+}
