067f3ade2ab9636bedefff1b32d2360181dde2d9
Arshad Chummun <arshad@chummun.com>
2019-04-19 13:00:43 +0400

    [DRUP-728] refactor and clean up configuration form

--- src/Installer/Form/ApigeeDevportalKickstartConfigurationForm.php
+++ src/Installer/Form/ApigeeDevportalKickstartConfigurationForm.php
@@ -21,26 +21,16 @@
 namespace Drupal\apigee_devportal_kickstart\Installer\Form;
 
 use Apigee\Edge\Api\Management\Controller\OrganizationController;
-use Apigee\Edge\Api\Management\Entity\OrganizationInterface;
 use Apigee\Edge\Api\Monetization\Controller\OrganizationProfileController;
 use Apigee\Edge\Api\Monetization\Controller\SupportedCurrencyController;
 use CommerceGuys\Addressing\AddressFormat\AddressField;
 use CommerceGuys\Addressing\AddressFormat\FieldOverride;
 use CommerceGuys\Intl\Currency\CurrencyRepository;
-use Drupal\apigee_edge\Form\AuthenticationForm;
 use Drupal\apigee_edge\SDKConnectorInterface;
-use Drupal\apigee_kickstart_m10n_add_credit\AddCreditConfig;
-use Drupal\commerce_price\CurrencyImporterInterface;
-use Drupal\commerce_price\Price;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Extension\MissingDependencyException;
-use Drupal\Core\Extension\ModuleInstallerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Messenger\MessengerInterface;
-use Drupal\key\KeyRepositoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -49,32 +39,11 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ApigeeDevportalKickstartConfigurationForm extends FormBase {
 
   /**
-   * The factory for configuration objects.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected $configFactory;
-
-  /**
    * SDK connector service.
    *
    * @var \Drupal\apigee_edge\SDKConnectorInterface
    */
   protected $sdkConnector;
-
-  /**
-   * The Drupal module installer.
-   *
-   * @var \Drupal\Core\Extension\ModuleInstallerInterface
-   */
-  protected $moduleInstaller;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
 
   /**
    * The library's currency repository.
@@ -112,33 +81,16 @@ class ApigeeDevportalKickstartConfigurationForm extends FormBase {
   protected $isMonetizable;
 
   /**
-   * @var \Drupal\key\KeyRepositoryInterface
-   */
-  protected $keyRepository;
-
-  /**
    * ApigeeM10nConfigurationForm constructor.
    *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The factory for configuration objects.
    * @param \Drupal\apigee_edge\SDKConnectorInterface $sdk_connector
    *   SDK connector service.
-   * @param \Drupal\Core\Extension\ModuleInstallerInterface $module_installer
-   *   The Drupal module installer.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager.
-   * @param \Drupal\key\KeyRepositoryInterface $key_repository
-   *   The key repository.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, SDKConnectorInterface $sdk_connector, ModuleInstallerInterface $module_installer, EntityTypeManagerInterface $entity_type_manager, LanguageManagerInterface $language_manager, KeyRepositoryInterface $key_repository) {
-    $this->configFactory = $config_factory;
+  public function __construct(SDKConnectorInterface $sdk_connector, LanguageManagerInterface $language_manager) {
     $this->sdkConnector = $sdk_connector;
-    $this->moduleInstaller = $module_installer;
-    $this->entityTypeManager = $entity_type_manager;
     $this->languageManager = $language_manager;
-    $this->keyRepository = $key_repository;
     $this->currencyRepository = new CurrencyRepository();
 
     try {
@@ -172,12 +124,8 @@ class ApigeeDevportalKickstartConfigurationForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('config.factory'),
       $container->get('apigee_edge.sdk_connector'),
-      $container->get('module_installer'),
-      $container->get('entity_type.manager'),
-      $container->get('language_manager'),
-      $container->get('key.repository')
+      $container->get('language_manager')
     );
   }
 
@@ -185,9 +133,12 @@ class ApigeeDevportalKickstartConfigurationForm extends FormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'apigee_m10n_configuration_form';
+    return 'apigee_devportal_kickstart_configuration_form';
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
     // Some messages stick around for installation tasks. Clear them all.
     $this->messenger()->deleteAll();
@@ -301,6 +252,20 @@ class ApigeeDevportalKickstartConfigurationForm extends FormBase {
       ],
     ];
 
+    // Add default address from organization.
+    if ($addresses = $this->organization->getAddresses()) {
+      /** @var \Apigee\Edge\Api\Monetization\Structure\Address $address */
+      $address = reset($addresses);
+
+      $form['store']['address']['#default_value'] = [
+        'address_line1' => $address->getAddress1(),
+        'locality' => $address->getCity(),
+        'administrative_area' => $address->getState(),
+        'country_code' => $address->getCountry(),
+        'postal_code' => $address->getZip(),
+      ];
+    }
+
     if (count($this->supportedCurrencies)) {
       $form['currencies'] = [
         '#type' => 'details',
@@ -330,20 +295,6 @@ class ApigeeDevportalKickstartConfigurationForm extends FormBase {
       ];
     }
 
-    // Add default address from organization.
-    if ($addresses = $this->organization->getAddresses()) {
-      /** @var \Apigee\Edge\Api\Monetization\Structure\Address $address */
-      $address = reset($addresses);
-
-      $form['store']['address']['#default_value'] = [
-        'address_line1' => $address->getAddress1(),
-        'locality' => $address->getCity(),
-        'administrative_area' => $address->getState(),
-        'country_code' => $address->getCountry(),
-        'postal_code' => $address->getZip(),
-      ];
-    }
-
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Save and continue'),
@@ -366,131 +317,31 @@ class ApigeeDevportalKickstartConfigurationForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    global $install_state;
     $form_state->cleanValues();
-    $install_state['m10n_config'] = $form_state->getValues();
+    $values = $form_state->getValues();
+    $values['modules'] = array_keys(array_filter($values['modules']));
 
-    // Update the supported currencies.
-    if (isset($install_state['m10n_config']['supported_currencies'])) {
-      foreach ($install_state['m10n_config']['supported_currencies'] as $currency_code) {
-        $install_state['m10n_config']['supported_currencies'][$currency_code] = $this->supportedCurrencies[strtolower($currency_code)];
-      }
-    }
-
-    // Add values for a payment gateway.
-    // TODO: Figure out if this should be configurable in the form.
-    $install_state['m10n_config']['gateway'] = [
-      'id' => 'default',
-      'label' => 'Default',
-      'plugin' => 'manual',
-    ];
-  }
-
-//  public function installModules(FormStateInterface $form_state, &$context) {
-//    if ($modules = array_keys(array_filter($form_state->getValue('modules')))) {
-//      // Enable the apigee_kickstart_m10n module also.
-//      // This holds all default config for m10n.
-//      $modules[] = 'apigee_kickstart_m10n';
-//
-//      // Enable the modules.
-//      try {
-//        $this->moduleInstaller->install($modules);
-//      } catch (MissingDependencyException $exception) {
-//        watchdog_exception('apigee_kickstart', $exception);
-//      }
-//    }
-//
-//    if (!isset($context['sandbox']['progress'])) {
-//      $context['sandbox']['progress'] = 0;
-//    }
-//
-//    $context['sandbox']['progress']++;
-//    $context['message'] = $this->t('Installed monetization modules');
-//  }
-
-//  public function importCurrencies(FormStateInterface $form_state, &$context) {
-//    if ($currencies = array_filter($form_state->getValue('supported_currencies'))) {
-//      /** @var CurrencyImporterInterface $currency_importer */
-//      // This cannot be injected because this has to be run after the required
-//      // modules is installed.
-//      $currency_importer = \Drupal::service('commerce_price.currency_importer');
-//
-//      foreach ($currencies as $currency_code) {
-//        // Import the currency.
-//        $currency_importer->import($currency_code);
-//
-//        // Save it to context.
-//        $context['results']['currencies'][$currency_code] = $this->supportedCurrencies[strtolower($currency_code)];
-//      }
-//    }
-//
-//    $context['sandbox']['progress']++;
-//    $context['message'] = $this->t('Imported supported currencies');
-//  }
-
-//  public function createStore(FormStateInterface $form_state, &$context) {
-//    // Create a store.
-//    $store = $this->entityTypeManager->getStorage('commerce_store')
-//      ->create($form_state->getValue('store'));
-//    $store->save();
-//
-//    // Create a payment gateway.
-//    $this->entityTypeManager->getStorage('commerce_payment_gateway')->create([
-//      'id' => 'default',
-//      'label' => 'Default',
-//      'plugin' => 'manual',
-//    ])->save();
-//
-//    // Save to context.
-//    $context['results']['store'] = $store;
-//
-//    $context['sandbox']['progress']++;
-//    $context['message'] = $this->t('Created a default store and payment gateway');
-//  }
-
-  public function createProducts(FormStateInterface $form_state, &$context) {
-    // If we have currencies and a store, create products.
-    if (count($context['results']['currencies']) && isset($context['results']['store'])) {
-      $add_credit_products = [];
-
-      /** @var \Apigee\Edge\Api\Monetization\Entity\SupportedCurrencyInterface $currency */
-      foreach ($context['results']['currencies'] as $currency) {
-        // Create a product variation for this currency.
-        $variation = $this->entityTypeManager->getStorage('commerce_product_variation')
-          ->create([
-            'type' => 'add_credit',
-            'sku' => "ADDCREDIT-{$currency->getName()}",
-            'title' => $currency->getName(),
-            'status' => 1,
-            'price' => new Price((string) $currency->getMinimumTopUpAmount(), $currency->getId()),
-          ]);
-        $variation->save();
-
-        // Create an add credit product for this currency.
-        $product = $this->entityTypeManager->getStorage('commerce_product')
-          ->create([
-            'title' => $currency->getName(),
-            'type' => 'add_credit',
-            'stores' => [$context['results']['store']],
-            'variations' => [$variation],
-            AddCreditConfig::ADD_CREDIT_ENABLED_FIELD_NAME => 1,
-          ]);
-        $product->save();
-
-        $add_credit_products[$currency->getId()] = [
-          'product_id' => $product->id(),
-        ];
+    if (count($values['modules'])) {
+      // Update the supported currencies.
+      if (isset($values['supported_currencies'])) {
+        foreach ($values['supported_currencies'] as $currency_code) {
+          $values['supported_currencies'][$currency_code] = $this->supportedCurrencies[strtolower($currency_code)];
+        }
       }
 
-      // Save config.
-      $this->configFactory()
-        ->getEditable(AddCreditConfig::CONFIG_NAME)
-        ->set('products', $add_credit_products)
-        ->save();
-    }
+      // Add values for a payment gateway.
+      // TODO: Figure out if this should be configurable in the form.
+      $values['gateway'] = [
+        'id' => 'default',
+        'label' => 'Default',
+        'plugin' => 'manual',
+      ];
 
-    $context['sandbox']['progress']++;
-    $context['message'] = $this->t('Created the default products');
+      // Save to install state.
+      $buildInfo = $form_state->getBuildInfo();
+      $buildInfo['args'][0]['m10n_config'] = $values;
+      $form_state->setBuildInfo($buildInfo);
+    }
   }
 
   /**
@@ -506,18 +357,15 @@ class ApigeeDevportalKickstartConfigurationForm extends FormBase {
     $install_state['completed_task'] = install_verify_completed_task();
   }
 
+  /**
+   * Helper to get importable currencies.
+   *
+   * @return array|\CommerceGuys\Intl\Currency\Currency[]
+   *   An array of importable currencies.
+   */
   protected function getImportableCurrencies() {
     $language = $this->languageManager->getConfigOverrideLanguage() ?: $this->languageManager->getCurrentLanguage();
     return $this->currencyRepository->getAll($language->getId());
-  }
-
-  protected function isMonetizable(OrganizationInterface $organization) {
-    // We can only check for monetization if we have a valid organization and
-    // a key configured.
-    if ($organization && ($active_key_id = $this->configFactory->get(AuthenticationForm::CONFIG_NAME)->get('active_key')) && ($this->keyRepository->getKey($active_key_id))) {
-      return $organization->getPropertyValue('features.isMonetizationEnabled') === 'true';
-    }
-    return FALSE;
   }
 
 }
