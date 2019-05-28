4a2ab7f8d1862b743f5d755b3d17ef77bec10764
Arshad Chummun <arshad@chummun.com>
2019-05-28 22:38:59 +0400

    Add an event subscriber to redirect to apidoc canonical page on not found exception

---
+++ modules/custom/apigee_kickstart_enhancement/src/EventSubscriber/PageNotFoundEventSubscriber.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_kickstart_enhancement\EventSubscriber;
+
+use Drupal\Core\Path\PathValidatorInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Handles not found exceptions for apidoc entities.
+ *
+ * @package Drupal\apigee_kickstart_enhancement\EventSubscriber
+ */
+class PageNotFoundEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The path validator service.
+   *
+   * @var \Drupal\Core\Path\PathValidatorInterface
+   */
+  protected $pathValidator;
+
+  /**
+   * PageNotFoundEventSubscriber constructor.
+   *
+   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
+   *   The path validator service.
+   */
+  public function __construct(PathValidatorInterface $path_validator) {
+    $this->pathValidator = $path_validator;
+  }
+
+  /**
+   * Redirects to the apidoc canonical route if we have a not found exception.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent $event
+   *   The exception event.
+   */
+  public function onNotFoundException(GetResponseForExceptionEvent $event) {
+    $path = NULL;
+
+    // Check if the request uri matches an apidoc canonical route.
+    // Also check for apidoc valid path.
+    if (!($event->getException() instanceof NotFoundHttpException)
+      || !(preg_match('/^\/api\/([0-9]+)\/.+$/', $event->getRequest()->getRequestUri(), $matches)
+        && ($path = "/api/{$matches[1]}")
+        && $this->pathValidator->isValid($path))
+    ) {
+      return;
+    }
+
+    // Redirect to the apidoc.
+    $event->setResponse(new RedirectResponse($path));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::EXCEPTION][] = ['onNotFoundException', 0];
+    return $events;
+  }
+
+}
