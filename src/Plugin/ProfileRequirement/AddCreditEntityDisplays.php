5ac98f3f81928d2fc81b990ce224ea4ca82886a5
Arshad Chummun <arshad@chummun.com>
2019-07-30 17:03:04 +0400

    [#233] Update the implementation for add credit entity displays

--- src/Plugin/ProfileRequirement/AddCreditEntityDisplays.php
+++ src/Plugin/ProfileRequirement/AddCreditEntityDisplays.php
@@ -20,6 +20,7 @@
 namespace Drupal\apigee_devportal_kickstart\Plugin\ProfileRequirement;
 
 use Drupal\Core\Config\FileStorage;
+use Drupal\Core\Config\StorageComparer;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\profile_requirement\Annotation\ProfileRequirement;
@@ -29,13 +30,18 @@ use Drupal\profile_requirement\Plugin\ProfileRequirementBase;
  * @ProfileRequirement(
  *   id="add_credit_entity_displays",
  *   label="Add credit entity displays",
- *   description="Configure default view displays for add credit products and checkout forms.",
+ *   description="Configure default view displays for add credit products and checkout forms. An add credit product type will be created if none is found.",
  *   severity="recommendation",
  *   action_button_label="Update displays",
  *   weight=100
  * )
  */
 class AddCreditEntityDisplays extends ProfileRequirementBase {
+
+  /**
+   * The name of the module.
+   */
+  const MODULE_NAME = 'apigee_kickstart_m10n_add_credit';
 
   /**
    * An array of config name.
@@ -50,10 +56,17 @@ class AddCreditEntityDisplays extends ProfileRequirementBase {
   public function __construct(array $configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->config = [];
-    foreach (['apigee_kickstart_m10n', 'apigee_kickstart_m10n_add_credit'] as $module) {
-      $path = drupal_get_path('module', $module) . '/config/optional';
-      $file_storage = new FileStorage($path);
-      foreach ($file_storage->listAll() as $name) {
+    $path = drupal_get_path('module', static::MODULE_NAME) . '/config/optional';
+    $file_storage = new FileStorage($path);
+    foreach ($file_storage->listAll() as $name) {
+      $this->config[$name] = $file_storage->read($name);
+    }
+
+    // If the module is installed, we only want all changed optional config.
+    if ($this->getModuleHandler()->moduleExists(static::MODULE_NAME)) {
+      $this->config = [];
+      $storage_comparer = new StorageComparer($file_storage, $this->getConfigStorage(), $this->getConfigManager());
+      foreach ($storage_comparer->createChangelist()->getChangelist('update') as $name) {
         $this->config[$name] = $file_storage->read($name);
       }
     }
@@ -78,7 +91,7 @@ class AddCreditEntityDisplays extends ProfileRequirementBase {
       '#title' => $this->t('Configuration'),
       '#type' => 'checkboxes',
       '#options' => array_combine(array_keys($this->config), array_keys($this->config)),
-      '#description' => $this->t('Import and overwrite the following configuration'),
+      '#description' => $this->t('Import and overwrite the following configuration.'),
       '#default_value' => array_keys($this->config),
     ];
     return $form;
@@ -88,6 +101,10 @@ class AddCreditEntityDisplays extends ProfileRequirementBase {
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    // Enable the apigee_kickstart_add_credit module if it is not enabled.
+    $this->getModuleInstaller()->install([static::MODULE_NAME]);
+
+    // Import config.
     $config = array_filter($form_state->getValue('config'));
     foreach ($config as $name) {
       $this->getConfigStorage()->write($name, $this->config[$name]);
@@ -98,14 +115,14 @@ class AddCreditEntityDisplays extends ProfileRequirementBase {
    * {@inheritdoc}
    */
   public function isApplicable(): bool {
-    return $this->getModuleHandler()->moduleExists('apigee_kickstart_m10n_add_credit');
+    return $this->getModuleHandler()->moduleExists('apigee_m10n_add_credit');
   }
 
   /**
    * {@inheritdoc}
    */
   public function isCompleted(): bool {
-    return FALSE;
+    return !count($this->config);
   }
 
 }
