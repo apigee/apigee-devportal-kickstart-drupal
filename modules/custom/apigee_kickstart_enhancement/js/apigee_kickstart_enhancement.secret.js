e28aa88f05cc68772779206ac5e3ebb79e078f53
Minnur Yunusov <mmyunusov@gmail.com>
2020-07-06 13:52:49 +0500

    [#416] New endpoint to get credentials via Ajax calls.

--- modules/custom/apigee_kickstart_enhancement/js/apigee_kickstart_enhancement.secret.js
+++ modules/custom/apigee_kickstart_enhancement/js/apigee_kickstart_enhancement.secret.js
@@ -19,7 +19,7 @@
  * @file
  * Script for the Secret element.
  */
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
 
   'use strict';
 
@@ -27,23 +27,41 @@
     attach: function (context, settings) {
       let $secret = $('.secret', context);
       if ($secret.length) {
-        $secret.each(function () {
+        $secret.each(function (i, element) {
           let $this = $(this);
+          let $el = $this.find('.secret__value');
+          let hClass = 'secret--hidden';
+          let appElWrapper = '.app-details-wrapper';
+          let $wrapper = $this.closest(appElWrapper);
+          let loader = drupalSettings.path.baseUrl + 'core/misc/throbber-active.gif';
 
           // Hide the value.
-          $this.addClass('secret--hidden');
+          $this.addClass(hClass);
 
           // Toggle secret.
           $(this).find('.secret__toggle').on('click', function (event) {
+            let index = $(this).closest(appElWrapper).find('.secret__toggle').index(this);
             event.preventDefault();
-            $this.toggleClass('secret--hidden')
+            $this.toggleClass(hClass);
+            if ($this.hasClass(hClass)) {
+              $el.html('');
+            }
+            else {
+              $el.html('<img src="' + loader + '" border="0" />');
+              getSecretValueAjax($wrapper.data('app'), function(data) {
+                $el.html(data[index]);
+              });
+            }
           });
 
           // Copy to clipboard.
           let $copy = $(this).find('.secret__copy');
           $copy.find('button').on('click', function (event) {
-            copyToClipboard($(this).data().value);
-            $copy.find('.badge').fadeIn().delay(1000).fadeOut();
+            let index = $(this).closest(appElWrapper).find('.secret__copy button').index(this);
+            getSecretValueAjax($wrapper.data('app'), function(data) {
+              copyToClipboard(data[index]);
+              $copy.find('.badge').fadeIn().delay(1000).fadeOut();
+            });
           })
         });
       }
@@ -74,6 +92,15 @@
         document.body.removeChild(textarea);
       }
     }
-  }
+  };
 
-})(jQuery, Drupal);
+  /**
+   * Get credentials based on the app name.
+   */
+  function getSecretValueAjax(app, callback) {
+    $.get( drupalSettings.path.baseUrl + 'admin/config/apigee-edge/app/' + app + '/credentials', function( data ) {
+      callback(data);
+    });
+  };
+
+})(jQuery, Drupal, drupalSettings);
