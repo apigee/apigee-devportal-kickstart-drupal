b102ce53594be0ec399b2b12e07cab9c95ec38a0
Arlina Espinoza <arlina.espinoza@gmail.com>
2020-09-28 13:39:34 -0700

    [#418] Code sniffer fixes.

--- .circleci/code-sniffer.sh
+++ .circleci/code-sniffer.sh
@@ -10,7 +10,7 @@ fi
 # Install dependencies and configure phpcs
 vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
 
-vendor/bin/phpmd modules/$1/src html cleancode,codesize,design,unusedcode --ignore-violations-on-exit --reportfile artifacts/phpmd/index.html
+vendor/bin/phpmd profiles/$1/src html cleancode,codesize,design,unusedcode --ignore-violations-on-exit --reportfile artifacts/phpmd/index.html
 vendor/bin/phpmetrics --extensions=php,inc,module --report-html=artifacts/phpmetrics --git modules/$1
 
 # Check coding standards
