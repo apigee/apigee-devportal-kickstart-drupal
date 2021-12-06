14f6d25863d5dc6662e2723ecb163c0c1f125d27
Hirenkumar Parmar <hirenkumar@google.com>
2021-12-06 20:43:22 +0530

    Update to theme related libraries with latest

--- themes/custom/apigee_kickstart/assets/js/apigee-kickstart.script.js
+++ themes/custom/apigee_kickstart/assets/js/apigee-kickstart.script.js
@@ -87,7 +87,7 @@ __webpack_require__.r(__webpack_exports__);
  // IE11 ponyfill.
 
 
-(0,css_vars_ponyfill__WEBPACK_IMPORTED_MODULE_3__.default)();
+(0,css_vars_ponyfill__WEBPACK_IMPORTED_MODULE_3__["default"])();
 
 /***/ }),
 
@@ -19849,12 +19849,14 @@ Popper.Defaults = Defaults;
 /******/ 			// add "moreModules" to the modules object,
 /******/ 			// then flag all "chunkIds" as loaded and fire callback
 /******/ 			var moduleId, chunkId, i = 0;
-/******/ 			for(moduleId in moreModules) {
-/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
-/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
 /******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
 /******/ 			}
-/******/ 			if(runtime) var result = runtime(__webpack_require__);
 /******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
 /******/ 			for(;i < chunkIds.length; i++) {
 /******/ 				chunkId = chunkIds[i];
